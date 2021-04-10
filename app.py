e022d823e98f099f278c46850893c8897223d71e
Viraj Gupta <61087588+Viraj24Gupta@users.noreply.github.com>
2021-04-11 01:59:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///finance1.db")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
